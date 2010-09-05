164304aa271f62bf0888e330484470a7432b56b5
Krzysztof Klinikowski <kkszysiu@gmail.com>
2010-09-05 23:34:08 +0200

    Sunshine should now work on twisted versions older than 0.9.0 but with disabled functions like avatar uploading and contact info.

--- sunshine/connection.py
+++ sunshine/connection.py
@@ -185,7 +185,8 @@ class SunshineConnection(telepathy.server.Connection,
             logger.info("We have %s contacts in file." % (self.configfile.get_contacts_count()))
             
             self.factory = GaduClientFactory(self.profile)
-            self.ggapi = GG_Oauth(self.profile.uin, parameters['password'])
+            if check_requirements() == True:
+                self.ggapi = GG_Oauth(self.profile.uin, parameters['password'])
             
             self._channel_manager = SunshineChannelManager(self)
 
@@ -201,7 +202,8 @@ class SunshineConnection(telepathy.server.Connection,
             SunshineAliasing.__init__(self)
             SunshineAvatars.__init__(self)
             SunshineCapabilities.__init__(self)
-            SunshineContactInfo.__init__(self)
+            if check_requirements() == True:
+                SunshineContactInfo.__init__(self)
             SunshineContacts.__init__(self)
             
             self.updateCapabilitiesCalls()
